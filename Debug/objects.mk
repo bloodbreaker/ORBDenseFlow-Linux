072de912b9e32cd63ce67f506814dd8b1677f2a7
Yan Zhang <yan.zhang@dkfz.heidelberg.de>
2015-08-29 21:09:09 +0200

    ORB performance test

--- Debug/objects.mk
+++ Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lopencv_core -lopencv_contrib -lopencv_xfeatures2d -lopencv_imgcodecs -lopencv_imgproc  -lopencv_highgui -lopencv_ml  -lopencv_video  -lopencv_features2d  -lopencv_calib3d  -lopencv_objdetect  -lopencv_flann
+LIBS := -lopencv_core -lopencv_xfeatures2d -lopencv_imgcodecs -lopencv_imgproc  -lopencv_highgui -lopencv_ml  -lopencv_video  -lopencv_features2d  -lopencv_calib3d  -lopencv_objdetect  -lopencv_flann
 
--- src/ORBDemo.cpp
+++ src/ORBDemo.cpp
@@ -13,7 +13,8 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgcodecs.hpp"
-#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/imgproc.hpp"
+//#include "opencv2/xfeature2d.hpp"
 
 using namespace cv;
 
@@ -22,14 +23,28 @@ int main(int argc, char** argv){
     // read images
     Mat img_1;
     Mat img_2;
+    Mat img_11;
+    Mat img_22;
     img_1 = imread(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
     img_2 = imread(argv[2],CV_LOAD_IMAGE_GRAYSCALE);
+
+    // resize the images
+//    Size size(512,512);
+//    resize(img_1,img_1,size);
+//    resize(img_2,img_2,size);
+
+    // rotate the images
+//    Point2f src_center(img_1.cols/2.0f, img_1.rows/2.0f);
+//    Mat rotation_matrix = getRotationMatrix2D(src_center, 90, 1.0);
+//    warpAffine(img_1,img_1,rotation_matrix,img_1.size());
+//    warpAffine(img_2,img_2,rotation_matrix,img_1.size());
+
 
     // now, you can no more create an instance on the 'stack', like in the tutorial
     // (yea, noticed for a fix/pr).
     // you will have to use cv::Ptr all the way down:
 
-    cv::Ptr<xfeatures2d::SIFT> f2d = xfeatures2d::SIFT::create();
+    cv::Ptr<Feature2D> f2d = ORB::create();
     // you get the picture, i hope..
 
     //-- Step 1: Detect the keypoints:
@@ -43,13 +58,14 @@ int main(int argc, char** argv){
     f2d->compute( img_2, keypoints_2, descriptors_2 );
 
     //-- Step 3: Matching descriptor vectors using BFMatcher :
-    BFMatcher matcher;
+    BFMatcher matcher(NORM_HAMMING,true);
     Mat match_visual;
 
     std::vector< DMatch > matches;
     matcher.match( descriptors_1, descriptors_2, matches );
     drawMatches(img_1, keypoints_1, img_2, keypoints_2, matches, match_visual,Scalar::all(-1), Scalar::all(-1),
-            std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+            std::vector<char>(), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+    namedWindow("matching results", WINDOW_AUTOSIZE);
     imshow("matching results", match_visual);
     waitKey(0);
     return 0;
